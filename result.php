f1e32c0b8d33b34160e41e89a444f42db4e228f3
ilyass.elassami <ilyass.elassami@external.engie.com>
2022-05-31 14:25:17 +0200

    fix foo loops

--- result.php
+++ result.php
@@ -9,7 +9,7 @@ $tab5 = [[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6]];
 
 // sort intervals by min offset.
 
-function sortIntervals(Array $tab){
+function sortIntervals(Array $tab):Array{
 	
 	$tab2 = [];
 	foreach($tab as $element){
@@ -28,7 +28,7 @@ function sortIntervals(Array $tab){
 
 
 // Union of interfaces
-function foo($tab){
+function foo(Array $tab):Array{
 	
 	$finalArray = [];
 	for( $i=0; $i< count($tab)-1; $i++){
@@ -37,12 +37,17 @@ function foo($tab){
 		for($j=$i+1;$j<count($tab); $j++){
 			var_dump("j: ".$j);
 			if($tempInterval[1]<$tab[$j][0]){
-				$i=$j-2;
+				if($j==count($tab)-1){
+					array_push($finalArray, $tempInterval,$tab[$j]);
+					break 2;
+				}else
+				$i= $j-1 ;
 				break;
 			}
 			else{
 				$tempInterval = [$tempInterval[0], max($tempInterval[1],$tab[$j][1])];
-				}
+				
+			}
 			}
 		array_push($finalArray, $tempInterval);
 	}
@@ -51,15 +56,19 @@ function foo($tab){
 echo "result of :[[0, 3], [6, 10]] :";
 echo "<br>";
 print_r(foo(sortIntervals($tab1)));
+echo "<br>";echo "<br>";
 echo "result of :[[0, 5], [3, 10]] :";
 echo "<br>";
 print_r(foo(sortIntervals($tab2)));
+echo "<br>";echo "<br>";
 echo "result of :[[0, 5], [2, 4]] :";
 echo "<br>";
 print_r(foo(sortIntervals($tab3)));
+echo "<br>";echo "<br>";
 echo "result of :[[7, 8], [3, 6], [2, 4]] :";
 echo "<br>";
 print_r(foo(sortIntervals($tab4)));
+echo "<br>";echo "<br>";
 echo "result of :[[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6]] :";
 echo "<br>";
 print_r(foo(sortIntervals($tab5)));
